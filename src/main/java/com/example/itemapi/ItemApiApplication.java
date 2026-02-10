695b0b260a0b78656e9cffdbdae4e543e8b93c61
shindeP2002 <111941440+shindeP2002@users.noreply.github.com>
2026-02-10 22:29:16 +0530

    Create ItemApiApplication.java

---
+++ src/main/java/com/example/itemapi/ItemApiApplication.java
@@ -0,0 +1,12 @@
+package com.example.itemapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItemApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItemApiApplication.class, args);
+    }
+}
