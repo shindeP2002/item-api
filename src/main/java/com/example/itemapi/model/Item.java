4d97154156f0493f35c5527b745c0f10364d990c
shindeP2002 <111941440+shindeP2002@users.noreply.github.com>
2026-02-10 22:27:00 +0530

    Create Item.java

---
+++ src/main/java/com/example/itemapi/model/Item.java
@@ -0,0 +1,59 @@
+package com.example.itemapi.model;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public class Item {
+
+    private int id;
+
+    @NotBlank(message = "Item name is required")
+    private String name;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @NotNull(message = "Price is required")
+    private Double price;
+
+    public Item() {}
+
+    public Item(int id, String name, String description, Double price) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
+}
